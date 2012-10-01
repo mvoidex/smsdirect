16833e1442e0fde0e68ce2f96ebed2f7a4e4d6b7
Alexandr Ruchkin <voidex@live.com>
2012-10-01 16:08:44 +0400

    Added doc.

--- src/SMSDirect.hs
+++ src/SMSDirect.hs
@@ -1,3 +1,12 @@
+-- | This library provides functions to SMSDirect API.
+--
+-- Simple usage:
+--
+-- @
+-- -- Get DB list
+-- r <- smsdirect "test" "test" getDB
+-- @
+--
 module SMSDirect (
   module SMSDirect.Command
   ) where
